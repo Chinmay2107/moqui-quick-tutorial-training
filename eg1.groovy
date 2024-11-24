27eb4fda3a94f5c4d4e39fea86319eca35229681
Chinmay Sharma <152295063+Chinmay2107@users.noreply.github.com>
2024-11-24 19:04:14 +0530

    Add files via upload

---
+++ eg1.groovy
@@ -0,0 +1,12 @@
+// Define a map to hold the result
+Map<String, Object> resultMap = [:]
+
+// Add random key-value pairs
+resultMap.put("key1", "value1")
+resultMap.put("key2", 12345)
+resultMap.put("key3", true)
+resultMap.put("key4", 3.14)
+resultMap.put("key5", ["nestedKey": "nestedValue"])
+
+// Return the map
+return resultMap
