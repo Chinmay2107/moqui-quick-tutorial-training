a010d9e93d272ea973ae4712859daeaacc34d04f
Chinmay Sharma <152295063+Chinmay2107@users.noreply.github.com>
2024-11-26 12:20:54 +0530

    Update eg1.groovy

--- eg1.groovy
+++ eg1.groovy
@@ -6,8 +6,8 @@ resultMap.put("key1", "value1")
 resultMap.put("key2", 12345)
 resultMap.put("key3", true)
 resultMap.put("key4", 3.14)
-System.out.println("\n\n=============-----"+ec.context.toString()+"--================\n\n")
-resultMap.put("Hello", inpara.get('Hello'))
+System.out.println("\n\n=============-----"+ec.context.toString()+"--========"+Hello+"========\n\n")
+resultMap.put("Hello", ec.context.get('Hello'))
 resultMap.put("key5", ["nestedKey": "nestedValue"])
 
 // Return the map
